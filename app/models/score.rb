fa7a0d7e4de78781a6d7cd7d791a587c74e68dc8
Bradley Hazelton <bradley@bradnet.net>
2017-07-06 20:40:34 -0400

    Seem to have working models. Final commit before feature branches

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,4 +2,29 @@ class Score < ApplicationRecord
   belongs_to :event
   belongs_to :segment
   belongs_to :score_type
+  
+  def name
+    if skater4 != ""
+      skater = "#{skater1}/#{skater2}/#{skater3}/#{skater4}"
+    elsif skater3 != ""
+      skater = "#{skater1}/#{skater2}/#{skater3}"
+    elsif skater2 != ""
+      skater = "#{skater1}/#{skater2}"
+    else
+      skater ="#{skater1}"
+    end
+  skater.split('/')
+  end
+
+  def isteam
+    if team_name == ""
+      team = false
+    elsif team_name
+      team = true
+    else
+      team = false
+    end
+    team
+  end
+  
 end
