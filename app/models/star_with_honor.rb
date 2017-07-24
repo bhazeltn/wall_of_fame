00e635eab5913d71fbd9eb51af4346c09141eb82
Bradley Hazelton <bradley@bradnet.net>
2017-07-24 14:07:03 -0600

    Corrected year in STAR Gold

--- app/models/star_with_honor.rb
+++ app/models/star_with_honor.rb
@@ -3,12 +3,6 @@ class StarWithHonor < ApplicationRecord
   belongs_to :star_test
   belongs_to :skater
   
-  # def test_level
-    
-  #   star_test.star_level.level + " " + star_test.name
-    
-  # end
-  
   private
   def update_year
     self.year = achieved.strftime("%Y")
